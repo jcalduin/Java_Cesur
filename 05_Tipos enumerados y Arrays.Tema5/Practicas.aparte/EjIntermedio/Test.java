73c22c90bfd4976d1918945ce8a44e2ec5dc154e
javi <javier.s408034@cesurformacion.com>
2024-12-07 16:03:47 +0100

    mejora ejercicio

--- 05_Tipos enumerados y Arrays.Tema5/Practicas.aparte/EjIntermedio/Test.java
+++ 05_Tipos enumerados y Arrays.Tema5/Practicas.aparte/EjIntermedio/Test.java
@@ -1,10 +1,13 @@
 package EjIntermedio;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
 
-        String mesIngresado = "febrero";
-        mesIngresado = mesIngresado.toUpperCase();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca un mes: ");
+        String mesIngresado = sc.nextLine().toUpperCase();
 
         Mes mesSeleccionado = obtenerMes(mesIngresado);
 
